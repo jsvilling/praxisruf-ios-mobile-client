0c19902897354b97d8b5029c052a75adf6da6274
Joshua Villing <jsvilling@gmail.com>
2021-10-17 14:15:50 +0200

    Initial commit

---
+++ praxisruf-ios-mobile-client/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  praxisruf-ios-mobile-client
+//
+//  Created by user on 17.10.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ praxisruf-ios-mobile-client/praxisruf_ios_mobile_clientApp.swift
@@ -0,0 +1,17 @@
+//
+//  praxisruf_ios_mobile_clientApp.swift
+//  praxisruf-ios-mobile-client
+//
+//  Created by user on 17.10.21.
+//
+
+import SwiftUI
+
+@main
+struct praxisruf_ios_mobile_clientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
