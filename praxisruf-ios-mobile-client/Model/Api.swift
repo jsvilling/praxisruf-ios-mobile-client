a8765a10698213ec23e44c4e13e6a9d40681bb86
Joshua Villing <jsvilling@gmail.com>
2021-10-18 16:32:00 +0200

    Draft login pic

--- praxisruf-ios-mobile-client/Model/Api.swift
+++ praxisruf-ios-mobile-client/Model/Api.swift
@@ -2,15 +2,47 @@
 //  Api.swift
 //  praxisruf-ios-mobile-client
 //
-//  Created by user on 18.10.21.
+//  Created by J. Villing on 18.10.21.
 //
 
 import Foundation
 
 class Api : ObservableObject {
     
-    @Published var result = String()
+    @Published var token = String()
     
+    let baseUrlValue = "https://www.praxisruf.ch/api"
     
-    
+    func login(username: String, password: String) {
+        print("Logging in with: \(username) \(password)")
+        guard let url = URL(string: "\(baseUrlValue)/users/login") else {
+            print("Invalid url")
+            return
+        }
+
+        let loginString = "\(username):\(password)"
+
+        guard let loginData = loginString.data(using: String.Encoding.utf8) else {
+            return
+        }
+        let base64LoginString = loginData.base64EncodedString()
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        request.setValue("Basic \(base64LoginString)", forHTTPHeaderField: "Authorization")
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let httpResponse = response as? HTTPURLResponse,(200...299).contains(httpResponse.statusCode) else {
+                print("Error reponse code ")
+                return
+            }
+            guard let receivedAuthToken = httpResponse.value(forHTTPHeaderField: "Authorization") else {
+                print("Empty token")
+                return
+            }
+            DispatchQueue.main.async {
+                self.token = receivedAuthToken
+            }
+        }.resume()
+    }
 }
--- praxisruf-ios-mobile-client/View/LoginView.swift
+++ praxisruf-ios-mobile-client/View/LoginView.swift
@@ -2,53 +2,17 @@
 //  ContentView.swift
 //  praxisruf-ios-mobile-client
 //
-//  Created by user on 17.10.21.
+//  Created by J. Villing on 17.10.21.
 //
 
 import SwiftUI
 
 struct LoginView: View {
-    
-    @State var username = ""
-    @State var password = ""
-    @State var isLoggedIn = false
-    
     var body: some View {
         VStack {
             WelcomeText()
             PraxisImage()
-            
-            List {
-                HStack {
-                    Image(systemName: "person")
-                    TextField(NSLocalizedString("username", comment: "username"), text: $username)
-                        .padding()
-                }
-                HStack {
-                    Image(systemName: "key")
-                    SecureField(NSLocalizedString("password", comment: "password"), text: $password)
-                            .padding()
-                }
-                NavigationLink(destination: IntercomView(), isActive: $isLoggedIn) {EmptyView()}.hidden()
-            }
-            .listStyle(PlainListStyle())
-            .frame(width: 440, height: 135)
-            .padding(.bottom, 40)
-            
-            Button(action: {
-                print("Logging in with \(self.username) \(self.password)")
-                self.isLoggedIn = true
-            }) {
-                Text(NSLocalizedString("login", comment: "login button text, all caps"))
-                    .font(.headline)
-                    .foregroundColor(.white)
-                    .padding()
-                    .frame(width: 220, height: 60)
-                    .background(Color.accentColor)
-                    .cornerRadius(15.0)
-            }
-        }
-        .padding()
+            LoginForm()
     }
 }
 
@@ -72,6 +36,45 @@ struct PraxisImage: View {
     }
 }
 
+struct LoginForm: View {
+    @State var username = ""
+    @State var password = ""
+    @State var isLoggedIn = false
+    
+    var body: some View {
+        List {
+            HStack {
+                Image(systemName: "person")
+                TextField(NSLocalizedString("username", comment: "username"), text: $username)
+                    .padding()
+            }
+            HStack {
+                Image(systemName: "key")
+                SecureField(NSLocalizedString("password", comment: "password"), text: $password)
+                        .padding()
+            }
+            NavigationLink(destination: IntercomView(), isActive: $isLoggedIn) {EmptyView()}.hidden()
+        }
+        .listStyle(PlainListStyle())
+        .frame(width: 440, height: 135)
+        .padding(.bottom, 40)
+        
+        Button(action: {
+            Api().login(username: self.username, password: self.password)
+            self.isLoggedIn = true
+        }) {
+            Text(NSLocalizedString("login", comment: "login button text, all caps"))
+                .font(.headline)
+                .foregroundColor(.white)
+                .padding()
+                .frame(width: 220, height: 60)
+                .background(Color.accentColor)
+                .cornerRadius(15.0)
+        }
+        .padding()
+    }
+}
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         NavigationView {
@@ -80,7 +83,6 @@ struct ContentView_Previews: PreviewProvider {
             }
         .navigationViewStyle(.stack)
         .previewDevice("iPad (9th generation)")
-
+        }
     }
-    
 }
