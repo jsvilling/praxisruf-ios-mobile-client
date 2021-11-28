1e6268f2d026a0dbe66b61cc4e88512ae2c8df5e
Joshua Villing <jsvilling@gmail.com>
2021-11-28 14:21:43 +0100

    Fix indentations

--- praxisruf-ios-mobile-client/Service/SpeechSynthesisService.swift
+++ praxisruf-ios-mobile-client/Service/SpeechSynthesisService.swift
@@ -13,44 +13,32 @@ class SpeechSynthesisService {
     func synthesize(notificationType: String, version: String) {
         let fileManager = FileManager.default
         let cacheUrl = fileManager.urls(for: .cachesDirectory, in: .userDomainMask)[0]
-        let destinationUrl = cacheUrl.appendingPathExtension("id1")
+        let destinationUrl = cacheUrl.appendingPathExtension("\(notificationType)-\(version)")
         
         if (fileManager.fileExists(atPath: destinationUrl.path)) {
             print("Speech Data is loadad from cache")
             AudioPlayer.playSounds(filePath: destinationUrl.path)
         } else {
-            
             print("Speech Data is loadd from server")
-        
-        
-        let defaults = UserDefaults.standard
-        guard let authToken = defaults.string(forKey: UserDefaultKeys.authToken) else {
-            print("No auth token found")
-            return
-        }
-        PraxisrufApi().synthesize(authToken: authToken, notificationType: notificationType) { result in
-            switch result {
-                case .success(let audioUrl):
-                
-                    
-                
-                
-                try? FileManager.default.removeItem(at: destinationUrl)
-                
-                do {
-                    try FileManager.default.copyItem(at: audioUrl, to: destinationUrl)
-                    AudioPlayer.playSounds(filePath: destinationUrl.path)
-                } catch let error {
-                    print(error)
+            let defaults = UserDefaults.standard
+            guard let authToken = defaults.string(forKey: UserDefaultKeys.authToken) else {
+                print("No auth token found")
+                return
+            }
+            PraxisrufApi().synthesize(authToken: authToken, notificationType: notificationType) { result in
+                switch result {
+                    case .success(let audioUrl):
+                        try? FileManager.default.removeItem(at: destinationUrl)
+                        do {
+                            try FileManager.default.copyItem(at: audioUrl, to: destinationUrl)
+                            AudioPlayer.playSounds(filePath: destinationUrl.path)
+                        } catch let error {
+                            print(error)
+                        }
+                    case .failure(let error):
+                        print(error.localizedDescription)
                 }
-                
-                
-                case .failure(let error):
-                    print(error.localizedDescription)
             }
         }
     }
-    }
-
-    
 }
