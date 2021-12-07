7c6ec6568c6d0e6f306e58d8ffbc349bb26f8f66
Joshua Villing <jsvilling@gmail.com>
2021-12-07 11:58:40 +0100

    Reduce duplication in PraxisrufApi

--- praxisruf-ios-mobile-client/Service/PraxisrufApi/PraxisrufApi.swift
+++ praxisruf-ios-mobile-client/Service/PraxisrufApi/PraxisrufApi.swift
@@ -19,102 +19,80 @@ class PraxisrufApi {
         
     let baseUrlValue = "https://www.praxisruf.ch/api"
                 
-    private func authorizedRequest(_ subUrl: String, method: String = "GET", task: @escaping (URLRequest) -> Void) {
+    func get<T>(_ subUrl: String, completion: @escaping (Result<T, PraxisrufApiError>) -> Void) where T : Decodable {
+        request(subUrl, completion: completion)
+    }
+    
+    func post<T>(_ subUrl: String, body: Data? = nil, completion: @escaping (Result<T, PraxisrufApiError>) -> Void) where T : Decodable {
+        request(subUrl, method: "POST", body: body, completion: completion)
+    }
+    
+    func delete(_ subUrl: String, completion: @escaping (Result<String, PraxisrufApiError>) -> Void) {
+        request(subUrl, method: "DELETE", completion: completion)
+    }
+    
+    private func request<T>(_ subUrl: String, method: String = "GET", body: Data? = nil, completion: @escaping (Result<T, PraxisrufApiError>) -> Void) where T : Decodable {
         guard let url = URL(string: "\(baseUrlValue)\(subUrl)") else {
-            fatalError("Invalid url configuration")
+            completion(.failure(.invalidData))
+            return
         }
                 
         guard let authToken = KeychainWrapper.standard.string(forKey: UserDefaultKeys.authToken) else {
-            fatalError("No authorization found")
+            completion(.failure(.invalidCredential))
+            return
         }
         
         var request = URLRequest(url: url)
         request.httpMethod = method
         request.addValue("Bearer \(authToken)", forHTTPHeaderField: "Authorization")
         
-        task(request)
-    }
-    
-    func get<T>(_ subUrl: String, completion: @escaping (Result<T, PraxisrufApiError>) -> Void) where T : Decodable {
-        authorizedRequest(subUrl) { request in
-            
-            URLSession.shared.dataTask(with: request) { data, response, error in
-                guard let httpResponse = response as? HTTPURLResponse,(200...299).contains(httpResponse.statusCode) else {
-                    completion(.failure(.errorResponse))
-                    return
-                }
-                
-                guard let responsData = data else {
-                    completion(.failure(.invalidData))
-                     return
-                 }
-                
-                guard let result = try? JSONDecoder().decode(T.self, from: responsData) else {
-                    completion(.failure(.invalidData))
-                    return
-                }
-            
-                completion(.success(result))
-            }.resume()
-            
+        if (body != nil) {
+            request.httpBody = body
+            request.setValue("application/json", forHTTPHeaderField: "Content-Type")
         }
-    }
-    
-    func post<T>(_ subUrl: String, body: Data? = nil, completion: @escaping (Result<T, PraxisrufApiError>) -> Void) where T : Decodable {
-        authorizedRequest(subUrl, method: "POST") { r in
-            var request = r
-            
-            if (body != nil) {
-                request.httpBody = body
-                request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let httpResponse = response as? HTTPURLResponse,(200...299).contains(httpResponse.statusCode) else {
+                completion(.failure(.errorResponse))
+                return
             }
             
-            URLSession.shared.dataTask(with: request) { data, response, error in
-                    guard let httpResponse = response as? HTTPURLResponse,(200...299).contains(httpResponse.statusCode) else {
-                        completion(.failure(.errorResponse))
-                        return
-                    }
-                
-                    guard let responsData = data else {
-                        completion(.failure(.invalidData))
-                         return
-                     }
-                
-                    guard let result = try? JSONDecoder().decode(T.self, from: responsData) else {
-                        completion(.failure(.invalidData))
-                        return
-                    }
-                
-                    completion(.success(result))
-                
-                
-            }.resume()
-        }
-    }
-    
-    func delete(_ subUrl: String, completion: @escaping (Result<String, PraxisrufApiError>) -> Void) {
-        authorizedRequest(subUrl) { request in
-            URLSession.shared.dataTask(with: request) { data, response, error in
-                guard let httpResponse = response as? HTTPURLResponse,(200...299).contains(httpResponse.statusCode) else {
-                    completion(.failure(.errorResponse))
-                    return
-                }
-                completion(.success("Success"))
-            }.resume()
-        }
+            guard let responsData = data else {
+                completion(.failure(.invalidData))
+                 return
+             }
+            
+            guard let result = try? JSONDecoder().decode(T.self, from: responsData) else {
+                completion(.failure(.invalidData))
+                return
+            }
+        
+            completion(.success(result))
+        }.resume()
     }
     
     func download(_ subUrl: String, completion: @escaping (Result<URL, PraxisrufApiError>) -> Void) {
-        authorizedRequest(subUrl) { request in
-            
-            URLSession.shared.downloadTask(with: request) { result, response, error in
-                guard let audioFileLocation = result else {
-                    completion(.failure(.custom(errorMessage: "No audio received")))
-                    return
-                }
-                completion(.success(audioFileLocation))
-            }.resume()
+        guard let url = URL(string: "\(baseUrlValue)\(subUrl)") else {
+            completion(.failure(.invalidData))
+            return
         }
+                
+        guard let authToken = KeychainWrapper.standard.string(forKey: UserDefaultKeys.authToken) else {
+            completion(.failure(.invalidCredential))
+            return
+        }
+        
+        var request = URLRequest(url: url)
+        request.addValue("Bearer \(authToken)", forHTTPHeaderField: "Authorization")
+        
+        URLSession.shared.downloadTask(with: request) { result, response, error in
+            guard let audioFileLocation = result else {
+                completion(.failure(.custom(errorMessage: "No audio received")))
+                return
+            }
+            completion(.success(audioFileLocation))
+        }.resume()
+        
     }
     
 }
