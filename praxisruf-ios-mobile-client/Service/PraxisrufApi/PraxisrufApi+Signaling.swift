27fb92eff2fa87d13a30bdec78fa0e71d2ea595c
Joshua Villing <jsvilling@gmail.com>
2021-12-18 20:44:17 +0100

    Create PraxisrufApi+Signaling

---
+++ praxisruf-ios-mobile-client/Service/PraxisrufApi/PraxisrufApi+Signaling.swift
@@ -0,0 +1,71 @@
+//
+//  PraxisrufApi+Signaling.swift
+//  praxisruf-ios-mobile-client
+//
+//  Created by J. Villing on 18.12.21.
+//
+
+import Foundation
+import SwiftKeychainWrapper
+
+extension PraxisrufApi {
+    
+    private static var websocket: URLSessionWebSocketTask? = nil;
+    
+    func connectSignalingServer(clientId: String) {
+        guard let authToken = KeychainWrapper.standard.string(forKey: UserDefaultKeys.authToken) else {
+            print("No authToken found")
+            return
+        }
+        let url = URL(string: "\(PraxisrufApi.webSocketBaseUrlValue)?clientId=\(clientId)")!
+        var request = URLRequest(url: url)
+        request.addValue("Bearer \(authToken)", forHTTPHeaderField: "Authorization")
+        let task = URLSession(configuration: .default).webSocketTask(with: request)
+        task.resume()
+        PraxisrufApi.websocket = task
+    }
+    
+    func pingSignalingConnection() {
+        PraxisrufApi.websocket?.sendPing() { error in
+            if (error != nil) {
+                print("Error")
+                return
+            }
+            print("Pong")
+        }
+    }
+    
+    func sendSignal(signal: Signal) {
+        let content = try? JSONEncoder().encode(signal)
+        let message = URLSessionWebSocketTask.Message.string(String(data: content!, encoding: .utf8)!)
+        PraxisrufApi.websocket?.send(message) { error in
+            if (error != nil) {
+                print("Error sending message")
+            }
+        }
+    }
+    
+    func listenForSignal(completion: @escaping (Signal) -> Void) {
+        guard let socket = PraxisrufApi.websocket else {
+            print("Signaling Server is not conneted")
+            return
+        }
+        socket.receive() { message in
+            switch(message) {
+                case .success(let content):
+                    switch(content) {
+                        case .string(let string):
+                            let signal = try? JSONDecoder().decode(Signal.self, from: string.data(using: .utf8)!)
+                            print("Received Signal with type \(signal!.type)")
+                            completion(signal!)
+                        default:
+                            fatalError("Invalid Signal received")
+                    }
+                case .failure(let error):
+                    print("Error")
+                    print(error)
+            }
+            self.listenForSignal(completion: completion)
+        }
+    }
+}
