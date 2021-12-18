4674a31ee13b55468bbb31f2f5cf99d2fc5d5210
Joshua Villing <jsvilling@gmail.com>
2021-12-18 22:10:23 +0100

    Simplify SessionDescription

--- praxisruf-ios-mobile-client/Model/Call/WebRTC/SessionDescription.swift
+++ praxisruf-ios-mobile-client/Model/Call/WebRTC/SessionDescription.swift
@@ -10,22 +10,14 @@ import WebRTC
 
 struct SessionDescription: Codable {
     let sdp: String
-    let type: SdpType
+    let rawType: Int
     
     init(from rtcSessionDescription: RTCSessionDescription) {
         self.sdp = rtcSessionDescription.sdp
-        
-        switch rtcSessionDescription.type {
-        case .offer:    self.type = .offer
-        case .prAnswer: self.type = .prAnswer
-        case .answer:   self.type = .answer
-        case .rollback: self.type = .rollback
-        @unknown default:
-            fatalError("Unknown RTCSessionDescription type: \(rtcSessionDescription.type.rawValue)")
-        }
+        self.rawType = rtcSessionDescription.type.rawValue
     }
     
     var rtcSessionDescription: RTCSessionDescription {
-        return RTCSessionDescription(type: self.type.rtcSdpType, sdp: self.sdp)
+        return RTCSessionDescription(type: RTCSdpType.init(rawValue: rawType)!, sdp: self.sdp)
     }
 }
