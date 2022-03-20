7067cd86296ae382fd735680a9f1058b6c08fafb
Joshua Villing <jsvilling@gmail.com>
2022-03-20 19:58:02 +0100

    Fix optional fields in

--- praxisruf-ios-mobile-client/Model/Configuration/NotificationType.swift
+++ praxisruf-ios-mobile-client/Model/Configuration/NotificationType.swift
@@ -11,8 +11,8 @@ struct NotificationType: IntercomItem, Hashable, Codable, Identifiable {
     let id: UUID
     let displayText: String
     let title: String
-    let body: String
-    let description: String
+    let body: String?
+    let description: String?
     
     static func compareByDisplayText(_ n1: NotificationType, _ n2: NotificationType) -> Bool {
         return n1.displayText < n2.displayText
