c33902d1e8f13dc27099bc061140d2c80f2ba4b3
Joshua Villing <jsvilling@gmail.com>
2022-03-21 08:26:11 +0100

    Add minimal animation on button press
    Received feedback, that is was iritating not to have any response after button press.

--- praxisruf-ios-mobile-client/View/Intercom/IntercomButton.swift
+++ praxisruf-ios-mobile-client/View/Intercom/IntercomButton.swift
@@ -13,16 +13,21 @@ struct IntercomButton<I>: View where I: IntercomItem {
     
     var body: some View {
   
-        Text(item.displayText)
-            .font(.system(size: 20, weight: .semibold, design: .rounded))
-            .frame(width: 200, height: 60)
-            .background(Color.white)
-            .clipShape(RoundedRectangle(cornerRadius: 16, style: .continuous))
-            .shadow(color: Color(#colorLiteral(red: 0.76, green: 0.81, blue: 0.92, alpha: 1)), radius: 20, x: 20, y: 20)
-            .shadow(color: Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)), radius: 20, x: -20, y: -20)
-            .onTapGesture {
-                action(item)
-            }
+        Button(action: onTap) {
+            Text(item.displayText)
+                .font(.system(size: 20, weight: .semibold, design: .rounded))
+                .foregroundColor(.black)
+        }
+        .scaleEffect()
+        .frame(width: 200, height: 60)
+        .background(Color.white)
+        .clipShape(RoundedRectangle(cornerRadius: 16, style: .continuous))
+        .shadow(color: Color(#colorLiteral(red: 0.76, green: 0.81, blue: 0.92, alpha: 1)), radius: 20, x: 20, y: 20)
+        .shadow(color: Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)), radius: 20, x: -20, y: -20)
+    }
+    
+    private func onTap() {
+        action(item)
     }
 }
 
